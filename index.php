56e26bed1e2b7b3cb34081c9060775581c330f10
kvothe <leon.adriand@gmail.com>
2020-07-05 00:55:26 -0500

    add table to index.php

--- index.php
+++ index.php
@@ -7,6 +7,23 @@
 	<link rel="stylesheet" href="">
 </head>
 <body>
-	
+	<a href="View/insert.php">Insert</a>
+	<table>
+		<caption>All tiendita</caption>
+		<thead>
+			<tr>
+				<th>Id</th>
+				<th>Producto</th>
+				<th>Precio</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>1</td>
+				<td>coca</td>
+				<td>$15</td>
+			</tr>
+		</tbody>
+	</table>
 </body>
 </html>
